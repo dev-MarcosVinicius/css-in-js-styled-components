310a1799b91b76a3de1b0af4fa2b52e07cf0cfb4
marcos.oliveira6 <marcosoliveira.ti9@gmail.com>
2021-12-11 11:29:37 -0300

    Troca para o component estilizado

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 // Importação do component estilizado
-// import { Form } from './src/styled-components/Form/';
+import { Form } from './src/styled-components/Form/';
 
 // Impotação do component não estilizado
-import { Form } from './src/normal-components/Form/';
+// import { Form } from './src/normal-components/Form/';
 
 export default function App() {
   return (
